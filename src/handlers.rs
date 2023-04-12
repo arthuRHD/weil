54ca034d903485bb941fa6c05aaaeb94f48cb523
Arthur Richard <arthur.richard@protonmail.com>
2023-04-13 00:21:49 +0200

    :tada: : Replace numeric ID with UUID (#6)

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,12 +1,22 @@
-use crate::models::{CreateUserRequest, User};
+use crate::models::{CreateUserRequest, ErrorMessage, User};
 use crate::repository::UserRepository;
-use actix_web::{web, HttpResponse, Responder};
-use diesel::{r2d2, result::Error as DieselError, PgConnection};
+use actix_web::{delete, get, patch, post, web, HttpResponse, Responder};
+use diesel::{
+    r2d2::{ConnectionManager, Pool},
+    result::Error as DieselError,
+    PgConnection,
+};
 use log::{error, info};
 
-// Create
+fn internal_error(e: diesel::result::Error) -> HttpResponse {
+    HttpResponse::InternalServerError().json(ErrorMessage {
+        reason: format!("{}", e),
+    })
+}
+
+#[post("/users")]
 pub async fn create_user(
-    pool: web::Data<r2d2::Pool<r2d2::ConnectionManager<PgConnection>>>,
+    pool: web::Data<Pool<ConnectionManager<PgConnection>>>,
     payload: web::Json<CreateUserRequest>,
 ) -> impl Responder {
     let mut conn = pool
@@ -15,21 +25,20 @@ pub async fn create_user(
 
     match UserRepository::create_user(&mut conn, &payload.username, &payload.email) {
         Ok(user) => {
-            info!("User created: {}", user.id);
+            info!("Inserted: {:?}", user);
             HttpResponse::Created().json(user)
         }
         Err(e) => {
-            error!("$e");
-            eprintln!("Failed to create user: {:?}", e);
-            HttpResponse::InternalServerError().finish()
+            error!("Failed to create user: {:?}", e);
+            internal_error(e)
         }
     }
 }
 
-// Read
+#[get("/users/{id}")]
 pub async fn get_user(
-    pool: web::Data<r2d2::Pool<r2d2::ConnectionManager<PgConnection>>>,
-    id: web::Path<i32>,
+    pool: web::Data<Pool<ConnectionManager<PgConnection>>>,
+    id: web::Path<String>,
 ) -> impl Responder {
     let mut conn = pool
         .get()
@@ -40,49 +49,81 @@ pub async fn get_user(
         Ok(None) => HttpResponse::NotFound().finish(),
         Err(e) => {
             eprintln!("Failed to get user: {:?}", e);
-            HttpResponse::InternalServerError().finish()
+            internal_error(e)
         }
     }
 }
 
-// Update
-pub async fn update_user(
-    pool: web::Data<r2d2::Pool<r2d2::ConnectionManager<PgConnection>>>,
-    id: web::Path<i32>,
-    user: web::Json<User>,
-) -> impl Responder {
+#[get("/users")]
+pub async fn get_users(pool: web::Data<Pool<ConnectionManager<PgConnection>>>) -> impl Responder {
     let mut conn = pool
         .get()
         .expect("failed to get a connection from the pool");
 
-    let mut updated_user = user.into_inner();
-    updated_user.id = id.into_inner();
-
-    match UserRepository::update_user(&mut conn, &updated_user) {
-        Ok(user) => HttpResponse::Ok().json(user),
-        Err(DieselError::NotFound) => HttpResponse::NotFound().finish(),
+    match UserRepository::count_users(&mut conn) {
+        Ok(number) => HttpResponse::Ok().json(number),
         Err(e) => {
-            eprintln!("Failed to update user: {:?}", e);
-            HttpResponse::InternalServerError().finish()
+            error!("Failed to fetch users: {:?}", e);
+            internal_error(e)
         }
     }
 }
 
-// Delete
-pub async fn delete_user(
-    pool: web::Data<r2d2::Pool<r2d2::ConnectionManager<PgConnection>>>,
-    id: web::Path<i32>,
+#[patch("/users/{id}")]
+pub async fn update_user(
+    pool: web::Data<Pool<ConnectionManager<PgConnection>>>,
+    id: web::Path<String>,
+    user: web::Json<CreateUserRequest>,
 ) -> impl Responder {
     let mut conn = pool
         .get()
         .expect("failed to get a connection from the pool");
 
-    match UserRepository::delete_user(&mut conn, id.into_inner()) {
-        Ok(1) => HttpResponse::NoContent().finish(),
+    match UserRepository::get_user(&mut conn, id.into_inner()) {
+        Ok(Some(fetched)) => {
+            let updated_user = User {
+                id: fetched.id,
+                username: user.username.clone(),
+                email: user.email.clone(),
+            };
+            match UserRepository::update_user(&mut conn, &updated_user) {
+                Ok(user) => {
+                    info!("Updated: {:?}", user);
+                    HttpResponse::Ok().json(user)
+                }
+                Err(DieselError::NotFound) => HttpResponse::NotFound().finish(),
+                Err(e) => {
+                    error!("Failed to update user: {:?}", e);
+                    internal_error(e)
+                }
+            }
+        }
+        Ok(None) => HttpResponse::NotFound().finish(),
+        Err(e) => {
+            eprintln!("Failed to get user: {:?}", e);
+            internal_error(e)
+        }
+    }
+}
+
+#[delete("/users/{id}")]
+pub async fn delete_user(
+    pool: web::Data<Pool<ConnectionManager<PgConnection>>>,
+    id: web::Path<String>,
+) -> impl Responder {
+    let mut conn = pool
+        .get()
+        .expect("failed to get a connection from the pool");
+    let id = id.into_inner();
+    match UserRepository::delete_user(&mut conn, id.clone()) {
+        Ok(1) => {
+            info!("Deleted user: {}", id);
+            HttpResponse::NoContent().finish()
+        }
         Ok(_) => HttpResponse::NotFound().finish(),
         Err(e) => {
             eprintln!("Failed to delete user: {:?}", e);
-            HttpResponse::InternalServerError().finish()
+            internal_error(e)
         }
     }
 }
@@ -91,14 +132,16 @@ pub async fn delete_user(
 async fn create_user_test() {
     use crate::create_connection_pool;
     use actix_web::{http::StatusCode, test, web, App};
+    use dotenv::dotenv;
     use serde_json::json;
 
+    dotenv().ok();
     env_logger::init();
 
     let app = test::init_service(
         App::new()
             .app_data(web::Data::new(create_connection_pool().clone()))
-            .service(web::resource("/users").route(web::post().to(create_user))),
+            .service(create_user),
     )
     .await;
 
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,35 @@
 extern crate diesel;
 extern crate dotenv;
 
-use dotenv::dotenv;
-
+use crate::config::*;
 use crate::handlers::*;
-use actix_web::{
-    error,
-    web::{self, JsonConfig},
-    App, HttpResponse, HttpServer,
-};
-use diesel::{
-    r2d2::{self, ConnectionManager},
-    PgConnection,
-};
+use actix_web::{web, App, HttpServer};
+
+use dotenv::dotenv;
 use std::env;
 
+mod config;
 mod handlers;
 mod models;
 mod repository;
 mod schema;
 
-fn create_connection_pool() -> r2d2::Pool<ConnectionManager<PgConnection>> {
-    dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let manager = ConnectionManager::<PgConnection>::new(database_url);
-    r2d2::Pool::builder()
-        .build(manager)
-        .expect("Failed to create connection pool")
-}
-
-fn create_json_config() -> JsonConfig {
-    web::JsonConfig::default()
-        .limit(4096)
-        .error_handler(|err, _| {
-            error::InternalError::from_response(err, HttpResponse::Conflict().into()).into()
-        })
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let connection_pool = web::Data::new(create_connection_pool());
-
+    dotenv().ok();
     env_logger::init();
+
+    let connection_pool = web::Data::new(create_connection_pool());
 
     HttpServer::new(move || {
         App::new()
             .app_data(connection_pool.clone())
             .app_data(create_json_config())
-            .service(
-                web::resource("/users/{id}")
-                    .route(web::get().to(get_user))
-                    .route(web::put().to(update_user))
-                    .route(web::delete().to(delete_user)),
-            )
-            .service(web::resource("/users").route(web::post().to(create_user)))
+            .service(get_user)
+            .service(get_users)
+            .service(update_user)
+            .service(delete_user)
+            .service(create_user)
     })
     .bind(env::var("SOCKET_ADDRESS").expect("Socket address must be set"))?
     .run()
